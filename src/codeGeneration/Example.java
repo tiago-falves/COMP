596b84077917d895643850de8cba287e96680e54
tiago-falves <tiago.falves@meo.pt>
2020-04-25 11:46:52 +0100

    Created Simple class

---
+++ src/codeGeneration/Example.java
@@ -0,0 +1,13 @@
+package codeGeneration;
+
+public class Example {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+        a = 5;
+        b=8;
+        c = a * b + a;
+
+    }
+}
