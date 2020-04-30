668977f3e56ecb97df4f630fc3dcf38d900d2b97
Pedro Esteves <up201705160@fe.up.pt>
2020-04-30 16:10:18 +0100

    Added line and column to simplenode

--- src/SemanticError.java
+++ src/SemanticError.java
@@ -13,9 +13,26 @@ public class SemanticError {
         this.numErrors = 0;
     }
 
+    private String getLineMessage(SimpleNode node){
+        if(node.jjtGetLine() == -1)
+            return "";
+
+        return "at line " + node.jjtGetLine();
+    }
+
+    private String getColumnMessage(SimpleNode node){
+        if(node.jjtGetColumn() == -1)
+            return "";
+        return ", column " + node.jjtGetColumn();
+    }
+
+    private String getPositionMessage(SimpleNode node){
+        return getLineMessage(node) + getColumnMessage(node);
+    }
+
     public void printError(SimpleNode node, String message) throws SemanticErrorException {
         this.numErrors++;
-        System.err.println("ERROR: " + message);
+        System.err.println("ERROR " + getPositionMessage(node) + ": " + message);
         
         if(numErrors >= maxNumErrors)
             throw new SemanticErrorException("Reached maximum number of semantic errors");
