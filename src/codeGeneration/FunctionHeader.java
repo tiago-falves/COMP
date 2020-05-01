971746410dca2d5d992a39c45879843ff7711f6b
tiago-falves <tiago.falves@meo.pt>
2020-05-01 11:13:17 +0100

    Main corrected

--- src/codeGeneration/FunctionHeader.java
+++ src/codeGeneration/FunctionHeader.java
@@ -6,12 +6,14 @@ import symbols.FunctionDescriptor;
 
 public class FunctionHeader {
     private static String INSTRUCTION = ".method public ";
+    private static String STATIC = "static ";
     private String methodName;
     private String methodDescriptor;
     private String returnType;
     private String STACK_LIMIT = "\t.limit stack ";
     private String LOCALS_LIMIT = "\t.limit locals ";
     private FunctionParameters functionParameters;
+    private FunctionDescriptor functionDescriptor;
 
 
     public FunctionHeader(FunctionDescriptor function){
@@ -19,6 +21,7 @@ public class FunctionHeader {
         this.methodName = function.getName();
         this.methodDescriptor = functionParameters.generate();
         this.returnType = CGConst.getJvmType(function);
+        this.functionDescriptor = function;
 
     }
 
@@ -28,6 +31,7 @@ public class FunctionHeader {
 
     public String generate() {
         String generatedCode = INSTRUCTION;
+        if(functionDescriptor.isStatic()) generatedCode+=STATIC;
         generatedCode += methodName + "(" + methodDescriptor + ")" + returnType + "\n";
         generatedCode += STACK_LIMIT + "\n" + LOCALS_LIMIT + "\n";
         return generatedCode;
