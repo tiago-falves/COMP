7f51439c7e61d005c498915da24b6a847047ea85
Pedro Esteves <up201705160@fe.up.pt>
2020-05-14 11:05:58 +0100

    Added if..else and while blocks llir

--- src/llir/LLIRIfElseBlock.java
+++ src/llir/LLIRIfElseBlock.java
@@ -1,5 +1,40 @@
 package llir;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LLIRIfElseBlock {
-    
+    public LLIRExpression ifExpression;
+    public List<LLIRNode> ifNodes;
+    public List<LLIRNode> elseNodes;
+
+    public LLIRIfElseBlock(){
+        ifExpression = null;
+        ifNodes = new ArrayList<>();
+        elseNodes = new ArrayList<>();
+    }
+
+    public void setExpression(LLIRExpression expression){
+        ifExpression = expression;
+    }
+
+    public void addIfNode(LLIRNode node){
+        ifNodes.add(node);
+    }
+
+    public void addElseNode(LLIRNode node){
+        elseNodes.add(node);
+    }
+
+    public LLIRExpression getExpression(){
+        return ifExpression;
+    }
+
+    public List<LLIRNode> getIfNodes(){
+        return ifNodes;
+    }
+
+    public List<LLIRNode> getElseNodes(){
+        return elseNodes;
+    }
 }
---
+++ src/llir/LLIRWhileBlock.java
@@ -0,0 +1,30 @@
+package llir;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LLIRWhileBlock {
+    private LLIRExpression whileExpression;
+    private List<LLIRNode> whileNodes;
+
+    public LLIRWhileBlock(){
+        whileExpression = null;
+        whileNodes = new ArrayList<>();
+    }
+
+    public void addNode(LLIRNode node){
+        whileNodes.add(node);
+    }
+
+    public void setExpression(LLIRExpression expression){
+        whileExpression = expression;
+    }
+
+    public List<LLIRNode> getNodes(){
+        return whileNodes;
+    }
+
+    public LLIRExpression getExpression(){
+        return whileExpression;
+    }
+}
