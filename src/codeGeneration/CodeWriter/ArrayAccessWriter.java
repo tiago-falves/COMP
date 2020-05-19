96c8f37288c66afca3d8d744903910a88887cb79
Nuno <up201706162@fe.up.pt>
2020-05-19 17:35:08 +0100

    Started arrays

---
+++ src/codeGeneration/CodeWriter/ArrayAccessWriter.java
@@ -0,0 +1,61 @@
+package codeGeneration.CodeWriter;
+
+import codeGeneration.CGConst;
+import codeGeneration.FunctionBody;
+import llir.LLIRInteger;
+import llir.LLIRArrayAccess;
+import symbols.Type;
+
+public class ArrayAccessWriter {
+    private String code;
+    private LLIRArrayAccess arrayAccess;
+
+    public ArrayAccessWriter(LLIRArrayAccess arrayAccess, String name){
+        this.code = "";
+        this.arrayAccess = arrayAccess;
+        this.code += generateArrayCode(arrayAccess.getArray(), name);
+        this.code += generateAccessCode(arrayAccess.getAccess(), name);
+    }
+
+    public String generateArrayCode(LLIRExpression expression, name) {
+        String result = new String();
+        if (expression instanceof LLIRMethodCall) {
+            MethodCallWriter methodCallWriter = new MethodCallWriter((LLIRMethodCall) expression);
+            result += methodCallWriter.getCode();
+        }
+        else if (expression instanceof LLIRArray) {
+            ArrayWriter arrayWriter = new ArrayWriter((LLIRArray) expression);
+            result += arrayWriter.getCode();
+        }
+
+        return result;
+    }
+
+    public String generateAccessCode(LLIRExpression expression, String name) {
+         String result = new String();
+        if(expression instanceof LLIRInteger) {
+            IntegerWriter integerWriter = new IntegerWriter((LLIRInteger) expression,name);
+            result += integerWriter.getCode();
+        }
+        else if(expression instanceof LLIRVariable) {
+            VariableWriter variableWriter = new VariableWriter((LLIRVariable) expression);
+            result += variableWriter.getCode();
+        }
+        else if (expression instanceof LLIRArithmetic) {
+            ArithmeticWriter arithmeticWriter = new ArithmeticWriter((LLIRArithmetic) expression,name);
+            result += arithmeticWriter.getCode();
+        }
+        else if (expression instanceof LLIRMethodCall) {
+            MethodCallWriter methodCallWriter = new MethodCallWriter((LLIRMethodCall) expression);
+            result += methodCallWriter.getCode();
+        }
+        else if (expression instanceof LLIRParenthesis) {
+            ExpressionWriter expressionWriter = new ExpressionWriter(((LLIRParenthesis) expression).getExpression(),name);
+            result += expressionWriter.getCode();
+        }
+    }
+
+    public String getCode(){
+        return this.code;
+    }
+}
