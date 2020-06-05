bee85bb6d84c8cb9a150d13d109ffbeb54edb498
Pedro Esteves <up201705160@fe.up.pt>
2020-06-05 15:21:22 +0100

    Added OptimizationManager

---
+++ src/optimizations/OptimizationManager.java
@@ -0,0 +1,9 @@
+package optimizations;
+
+public class OptimizationManager {
+    public static boolean constantPropagation = false;
+    public static boolean constantFolding = false;
+
+    public static boolean reducedLocals = false;
+    public static int maximumLocalVariables;
+}
